2851eb8920d392ecc42a01ccfc92796d966b9d6d
Mehdi <zeggai.mehdi@yahoo.fr>
2023-06-07 12:29:21 +0200

    ecercice-03

---
+++ exercices-03/exercice-03-01.py
@@ -0,0 +1,16 @@
+# exo 3.1
+# Alice est née le 10 février 1988.
+# Quelle âge a-t-elle cette année ?
+# Ne tenez pas compte du mois, on va partir du principe qu'on est après le 10 février pour ne pas se compliquer la vie.
+# Stockez l'année en cours dans une variable nommée `year`.
+# Calculez l'âge d'Alice en utilisant les variables `birthyear` et `year` puis stockez le résultat dans une variable nommée `age` et affichez ce résultat.
+
+birthyear = 1988
+
+# réponse 3.1
+year = 2023
+birthyear = 1998
+age=(year-birthyear)
+age=(2023-1998)
+print(2023-1998)
+
