9f6a3b477c53b6da6665e3cf8c5cf34e2d8fd6e9
Mehdi <zeggai.mehdi@yahoo.fr>
2023-06-13 16:56:20 +0200

    excercice5

---
+++ exercices-05/exercice-05-01.py
@@ -0,0 +1,10 @@
+# exo 5.1
+# Ajoutez de la documentation à la fonction suivante et vérifiez
+# qu'elle s'affiche correctement en utilisant la fonction help()
+
+# réponse 5.1
+
+def multiplication(a: float, b: float) -> float:
+    return a * b
+
+print((a:float,b:float)->float)
